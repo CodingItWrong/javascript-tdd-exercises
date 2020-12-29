1d10f624fa8d51a8abf41194a83b724f7cc4559c
Josh Justice <me@codingitwrong.com>
2020-12-29 17:14:46 -0500

    Convert 1 to I

---
+++ roman-numerals/src/romanNumerals.spec.js
@@ -0,0 +1,11 @@
+function numberToRomanNumeral() {
+  return 'I';
+}
+
+describe('romanNumerals', () => {
+  describe('numberToRomanNumeral', () => {
+    it('converts 1 to I', () => {
+      expect(numberToRomanNumeral(1)).toEqual('I');
+    });
+  });
+});
