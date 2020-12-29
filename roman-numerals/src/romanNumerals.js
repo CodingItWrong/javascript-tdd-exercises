66b91833ba39839a0670ec9cd5cb7bd0e49a8890
Josh Justice <me@codingitwrong.com>
2020-12-29 17:53:17 -0500

    Combine 9 and 10 case

--- roman-numerals/src/romanNumerals.js
+++ roman-numerals/src/romanNumerals.js
@@ -1,10 +1,11 @@
 export function numberToRomanNumeral(number) {
   let numeral = '';
 
-  if (number === 10) {
+  if (number >= 9) {
+    if (number === 9) {
+      numeral += 'I';
+    }
     numeral += 'X';
-  } else if (number === 9) {
-    numeral += 'IX';
   } else if (number >= 4) {
     if (number === 4) {
       numeral += 'I';
