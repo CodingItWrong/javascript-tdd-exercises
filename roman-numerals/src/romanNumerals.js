3fa086f266a21f7bc147695a061641b81073862e
Josh Justice <me@codingitwrong.com>
2021-01-05 17:20:03 -0500

    Extract duplicate logic to function

--- roman-numerals/src/romanNumerals.js
+++ roman-numerals/src/romanNumerals.js
@@ -1,28 +1,42 @@
 const NUMERAL_ONE = 'I';
 
 // TODO: need names that don't mention the values
-const FIVE_THRESHOLD = 5;
-const TEN_THRESHOLD = 10;
+const FIVE_CONFIG = {
+  threshold: 5,
+  character: 'V',
+};
+const TEN_CONFIG = {
+  threshold: 10,
+  character: 'X',
+};
+
+function handleThresholdCase({number, config}) {
+  const {threshold, character} = config;
+
+  let numeral = '';
+  if (number === threshold - 1) {
+    numeral += NUMERAL_ONE;
+  }
+  numeral += character;
+  if (number > threshold) {
+    numeral += numberToRomanNumeral(number - threshold);
+  }
+  return numeral;
+}
 
 export function numberToRomanNumeral(number) {
   let numeral = '';
 
-  if (number >= TEN_THRESHOLD - 1) {
-    if (number === TEN_THRESHOLD - 1) {
-      numeral += NUMERAL_ONE;
-    }
-    numeral += 'X';
-    if (number > TEN_THRESHOLD) {
-      numeral += numberToRomanNumeral(number - TEN_THRESHOLD);
-    }
-  } else if (number >= FIVE_THRESHOLD - 1) {
-    if (number === FIVE_THRESHOLD - 1) {
-      numeral += NUMERAL_ONE;
-    }
-    numeral += 'V';
-    if (number > FIVE_THRESHOLD) {
-      numeral += numberToRomanNumeral(number - FIVE_THRESHOLD);
-    }
+  if (number >= TEN_CONFIG.threshold - 1) {
+    numeral += handleThresholdCase({
+      number,
+      config: TEN_CONFIG,
+    });
+  } else if (number >= FIVE_CONFIG.threshold - 1) {
+    numeral += handleThresholdCase({
+      number,
+      config: FIVE_CONFIG,
+    });
   } else {
     for (let i = 0; i < number; i += 1) {
       numeral += NUMERAL_ONE;
